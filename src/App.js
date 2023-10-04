303e6af1482b90c2d89936c6bbce50177a1ef3b8
caudrel <lozachaurelie@gmail.com>
2023-10-04 12:04:09 +0200

    working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import DisplayEmployee from "./components/DisplayEmployee";
+import axios from "axios";
+import { useState } from "react";
+
+const sampleEmployee = {
+  gender: "male",
+  name: {
+    first: "Charlie",
+    last: "Thompson",
+  },
+  location: {
+    street: {
+      number: 761,
+      name: "Tay Street",
+    },
+    city: "Timaru",
+    postcode: 76111,
+  },
+  email: "charlie.thompson@example.com",
+  picture: {
+    medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+  },
+};
 
 function App() {
+  const getEmployee = () => {
+    // Send the request
+    axios
+      .get("https://randomuser.me/api?nat=en")
+      // Use this data to update the state
+      .then((response) => {
+        console.log(response.data);
+        setEmployee(response.data.results[0]);
+      });
+  };
+
+  const [employee, setEmployee] = useState(sampleEmployee);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <DisplayEmployee employee={employee} />
+      <button type="button" onClick={getEmployee}>
+        Get employee
+      </button>
+    </>
   );
 }
 
